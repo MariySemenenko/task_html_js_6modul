d8abade762144ea16f1aa6d9cb585075853a0970
MariySemenenko <mashyni92@gmail.com>
2023-03-17 13:14:21 +0200

    task 2

--- task.js/modul6.js
+++ task.js/modul6.js
@@ -30,3 +30,18 @@
 //   //reset очищає форму після кожного введення
 //   event.currentTarget.reset();
 // }
+///////////////////////////////////////////
+
+/достукуємось до двох елементів
+const container = document.querySelector('.container');
+const footer = document.querySelector('footer');
+
+//створимо h1
+const h1 = document.createElement('h1');
+
+// додаємо h1 текст
+h1.textContent = 'Hello world!';
+
+//h1 добавляю до container
+container.append(h1);
+
